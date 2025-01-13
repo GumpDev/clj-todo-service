124136cc876ee9085d4d63aa3bf3bb17be2b213c
gump <contato@gump.dev>
2025-01-12 21:06:42 -0300

    added mem config

--- src/clj_todo_service/core.clj
+++ src/clj_todo_service/core.clj
@@ -2,9 +2,11 @@
   (:require [datomic.client.api :as d]))
 
 (def client (d/client {:server-type :datomic-local
+                       :storage-dir :mem
                        :system "dev"}))
 
 (d/create-database client {:db-name "teste"})
-(def conn (d/connect client {:db-name "tutorial"}))
+(def conn (d/connect client {:db-name "teste"}))
+
 
 (println "Started App")
